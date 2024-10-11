126c1f69fd1c82ce760e656a4e2b66f2553000ae
Walter Juan <4141614+walter-juan@users.noreply.github.com>
2024-10-12 00:02:11 +0200

    Update octicons icons

--- octicons/src/commonMain/kotlin/com/woowla/compose/icon/collections/octicons/octicons/PinSlash16.kt
+++ octicons/src/commonMain/kotlin/com/woowla/compose/icon/collections/octicons/octicons/PinSlash16.kt
@@ -1,9 +1,12 @@
 package com.woowla.compose.icon.collections.octicons.octicons
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType
 import androidx.compose.ui.graphics.PathFillType.Companion.NonZero
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.StrokeCap.Companion.Butt
+import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.StrokeJoin.Companion.Miter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.ImageVector.Builder
