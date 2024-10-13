1dacc8973d155d92497191f497320ee8e23312d2
Walter Juan <4141614+walter-juan@users.noreply.github.com>
2024-10-13 16:38:15 +0200

    Update sample

--- sample/composeApp/src/desktopMain/kotlin/com/woowla/compose/icon/collections/sample/main.kt
+++ sample/composeApp/src/desktopMain/kotlin/com/woowla/compose/icon/collections/sample/main.kt
@@ -1,9 +1,5 @@
 package com.woowla.compose.icon.collections.sample
 
-import androidx.compose.foundation.VerticalScrollbar
-import androidx.compose.foundation.rememberScrollbarAdapter
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
@@ -12,11 +8,6 @@ fun main() = application {
         onCloseRequest = ::exitApplication,
         title = "Compose icon collections",
     ) {
-        App { listState ->
-            VerticalScrollbar(
-                modifier = Modifier.align(Alignment.CenterEnd),
-                adapter = rememberScrollbarAdapter(scrollState = listState)
-            )
-        }
+        App()
     }
 }
